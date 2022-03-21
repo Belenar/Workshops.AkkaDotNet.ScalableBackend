ab036ec5f56531e89a8104480d2f94efd3b6ba99
Hannes Lowette <Hannes.Lowette@axxes.com>
2022-03-21 21:43:02 +0100

    Fix Obsolete RemoteCommandsPalette

--- src/Axxes.AkkaDotNet.Workshop.SeedNode/SeedNodeService.cs
+++ src/Axxes.AkkaDotNet.Workshop.SeedNode/SeedNodeService.cs
@@ -29,7 +29,7 @@ namespace Axxes.AkkaDotNet.Workshop.SeedNode
             // enable Akka.Cluster management commands
             pbm.RegisterCommandPalette(ClusterCommands.Instance);
             // enable Akka.Remote management commands
-            pbm.RegisterCommandPalette(RemoteCommands.Instance);
+            pbm.RegisterCommandPalette(new RemoteCommands());
             pbm.Start();
         }
 
