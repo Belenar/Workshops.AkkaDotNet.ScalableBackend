3b556c2e236d0ba1eff64db253974e8e1642202c
Hannes Lowette <Hannes.Lowette@axxes.com>
2022-03-23 15:14:51 +0100

    Normalization step 1

--- src/Axxes.AkkaDotNet.Workshop.ClusterNode/Actors/DeviceActor.cs
+++ src/Axxes.AkkaDotNet.Workshop.ClusterNode/Actors/DeviceActor.cs
@@ -1,20 +1,38 @@
 ﻿using System;
 using Akka.Actor;
+using Akka.Event;
+using Axxes.AkkaDotNet.Workshop.Shared.Messages;
 
 namespace Axxes.AkkaDotNet.Workshop.ClusterNode.Actors;
 
-internal class DeviceActor : UntypedActor
+internal class DeviceActor : ReceiveActor
 {
+    private const string ValueNormalizationName = "value-normaization";
     private readonly Guid _deviceId;
 
     public DeviceActor(Guid deviceId)
     {
         _deviceId = deviceId;
+        Receive<MeterReadingReceived>(HandleMeterReadingReceived);
+        Receive<NormalizedMeterReading>(HandleNormalizedMeterReadingReceived);
+        CreateChildren();
     }
 
-    protected override void OnReceive(object message)
+    private void CreateChildren()
     {
-        throw new NotImplementedException();
+        // value-normalization
+        var props = ValueNormalizationActor.CreateProps();
+        Context.ActorOf(props, ValueNormalizationName);
+    }
+
+    private void HandleMeterReadingReceived(MeterReadingReceived message)
+    {
+        Context.Child(ValueNormalizationName).Tell(message);
+    }
+
+    private void HandleNormalizedMeterReadingReceived(NormalizedMeterReading message)
+    {
+        Context.GetLogger().Info($"Device {_deviceId} - Time: {message.Timestamp} - Reading: {message.MeterReading}");
     }
 
     public static Props CreateProps(Guid deviceId)
---
+++ src/Axxes.AkkaDotNet.Workshop.ClusterNode/Actors/ValueNormalizationActor.cs
@@ -0,0 +1,30 @@
+﻿using Akka.Actor;
+using Axxes.AkkaDotNet.Workshop.ClusterNode.Helpers;
+using Axxes.AkkaDotNet.Workshop.Shared.Messages;
+
+namespace Axxes.AkkaDotNet.Workshop.ClusterNode.Actors;
+
+internal class ValueNormalizationActor : ReceiveActor
+{
+    private readonly ValueNormalizationHelper _helper = new();
+
+    public ValueNormalizationActor()
+    {
+        Receive<MeterReadingReceived>(HandleMeterReadingReceived);
+    }
+
+    private void HandleMeterReadingReceived(MeterReadingReceived message)
+    {
+        var normalizedReadings = _helper.GetNormalizedReadingsUntil(message.TimestampUtc, message.MeterReading);
+
+        foreach (var normalizedMeterReading in normalizedReadings)
+        {
+            Context.Parent.Tell(normalizedMeterReading);
+        }
+    }
+
+    public static Props CreateProps()
+    {
+        return Props.Create<ValueNormalizationActor>();
+    }
+}
