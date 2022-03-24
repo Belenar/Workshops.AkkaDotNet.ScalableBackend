bf0b9e394bc336684b7ed3ab1ac1fe9dd2860178
Hannes Lowette <Hannes.Lowette@axxes.com>
2022-03-24 09:03:31 +0100

    More storage

--- src/Axxes.AkkaDotNet.Workshop.ClusterNode/Actors/ValueStorageActor.cs
+++ src/Axxes.AkkaDotNet.Workshop.ClusterNode/Actors/ValueStorageActor.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.Persistence;
+using Axxes.AkkaDotNet.Workshop.ClusterNode.State;
 using Axxes.AkkaDotNet.Workshop.Shared.Messages;
 
 namespace Axxes.AkkaDotNet.Workshop.ClusterNode.Actors
@@ -13,15 +10,34 @@ namespace Axxes.AkkaDotNet.Workshop.ClusterNode.Actors
     {
         public override string PersistenceId { get; }
 
+        private NormalizedReadingPersistenceState _state = new();
+
         public ValueStorageActor(Guid deviceId)
         {
             PersistenceId = $"value-storage-{deviceId}";
+            Recover<SnapshotOffer>(RestoreSnapshot);
+            Recover<NormalizedMeterReading>(msg => _state.Add(msg));
             Command<NormalizedMeterReading>(HandleNormalizedMeterReading);
+            Command<TakeHourlySnapshot>(msg => SaveSnapshot(_state));
+            
+
+            Context.System.Scheduler.ScheduleTellRepeatedly(
+                TimeSpan.FromSeconds(10),
+                TimeSpan.FromHours(1),
+                Self,
+                new TakeHourlySnapshot(),
+                Self);
+        }
+
+        private void RestoreSnapshot(SnapshotOffer offer)
+        {
+            var snapshot = (NormalizedReadingPersistenceState)offer.Snapshot;
+            _state = snapshot;
         }
 
         private void HandleNormalizedMeterReading(NormalizedMeterReading message)
         {
-            Persist(message, msg => { });
+            Persist(message, msg => _state.Add(msg));
         }
 
         public static Props CreateProps(Guid deviceId)
