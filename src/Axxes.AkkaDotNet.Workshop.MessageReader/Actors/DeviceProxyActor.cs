b307cb90620d2f3719bd9f95b685e018b5394caf
Hannes Lowette <lowette.hannes@gmail.com>
2022-11-15 16:54:25 -0600

    Attempt to make the proxy more robust

--- src/Axxes.AkkaDotNet.Workshop.MessageReader/Actors/DeviceProxyActor.cs
+++ src/Axxes.AkkaDotNet.Workshop.MessageReader/Actors/DeviceProxyActor.cs
@@ -6,8 +6,11 @@ namespace Axxes.AkkaDotNet.Workshop.MessageReader.Actors;
 
 internal class DeviceProxyActor : ReceiveActor, IWithUnboundedStash
 {
+    public record RetryConnect;
+
     private readonly Guid _deviceId;
     private IActorRef _deviceActor;
+    private Cancelable _cancelRetries;
 
     public DeviceProxyActor(Guid deviceId)
     {
@@ -19,6 +22,7 @@ internal class DeviceProxyActor : ReceiveActor, IWithUnboundedStash
     {
         Receive<DeviceConnected>(HandleDeviceConnected);
         Receive<MeterReadingReceived>(StashMeterReading);
+        Receive<RetryConnect>(_ => ConnectDevice());
     }
 
     private void Initialized()
@@ -29,12 +33,33 @@ internal class DeviceProxyActor : ReceiveActor, IWithUnboundedStash
     private void HandleDeviceConnected(DeviceConnected deviceConnected)
     {
         _deviceActor = deviceConnected.DeviceActor;
+        _cancelRetries.Cancel();
         Become(Initialized);
         Stash.UnstashAll();
     }
 
     protected override void PreStart()
     {
+        ConnectDevice();
+
+        // Schedule a check message that will retry the connect every 30 seconds
+        _cancelRetries = new Cancelable(Context.System.Scheduler);
+
+        var delay = new TimeSpan(0, 0, 30);
+        Context.System.Scheduler.ScheduleTellRepeatedly(
+            initialDelay: delay,
+            interval: delay,
+            sender: Self,
+            receiver: Self,
+            message: new RetryConnect(),
+            cancelable: _cancelRetries);
+    }
+
+    private void ConnectDevice()
+    {
+        if (_deviceActor != null)
+            return;
+
         // Don't use hardcoded strings in PROD!
         var selection = Context.ActorSelection("akka.tcp://WorkshopActorSystem@localhost:8081/user/device-manager");
         selection.Tell(new ConnectDevice(_deviceId));
