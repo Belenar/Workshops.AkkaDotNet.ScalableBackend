bead9130686a3119e94ec9d45d11ec37879acf52
Hannes Lowette <lowette.hannes@gmail.com>
2022-11-16 20:14:53 +0100

    Get connectionstring from Config

--- src/Axxes.AkkaDotNet.Workshop.ClusterNode/Program.cs
+++ src/Axxes.AkkaDotNet.Workshop.ClusterNode/Program.cs
@@ -1,21 +1,23 @@
+using Axxes.AkkaDotNet.Workshop.ClusterNode.Helpers;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Axxes.AkkaDotNet.Workshop.ClusterNode
+namespace Axxes.AkkaDotNet.Workshop.ClusterNode;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
-                {
-
-                    services.AddHostedService<ClusterNodeService>();
-                });
+        CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                DatabaseConnectionFactory.HistoryConnectionString =
+                    hostContext.Configuration.GetSection("ConnectionStrings").GetValue<string>("HistoryConnectionString");
+                services.AddHostedService<ClusterNodeService>();
+            });
 }
