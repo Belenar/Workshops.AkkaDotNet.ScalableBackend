793f79b9e00e5e568851fbbe54d5f624713c0996
Hannes Lowette <lowette.hannes@gmail.com>
2022-11-16 20:04:11 +0100

    DbWriterActor

---
+++ src/Axxes.AkkaDotNet.Workshop.ClusterNode/Actors/Device/ReadingDbWriterActor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axxes.AkkaDotNet.Workshop.ClusterNode.Actors.Device
+{
+    internal class ReadingDbWriterActor
+    {
+    }
+}
